194c24903413161895a5f37a0df5324933f917b6
jleme654 <julioromao3@gmail.com>
2014-05-19 21:08:20 -0300

    Teste primeiro

---
+++ src/main/java/com/mycompany/practica1/Main.java
@@ -0,0 +1,14 @@
+package com.mycompany.practica1;
+
+/**
+ * @author Julio Leme
+ * @date   19/05/2014
+ *
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Julio Leme");
+    }
+
+}
